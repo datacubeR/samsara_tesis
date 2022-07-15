383dfd514cfea1e9471df08c85d7b577d2946759
Alfonso Tobar <big_drummer2@hotmail.com>
2022-07-15 15:39:19 -0400

    Análisis de Resample

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,24 +1,8 @@
-import pandas as pd
+import random
+
 import matplotlib.pyplot as plt
-
-def import_file(file, id_length = 4):
-    """Importa todos los archivos CSV de prueba convirtiendolo en un sólo DataFrame.
-
-    Parameters
-    ----------
-    file : str
-        Path que apunta a alguno de los archivos CSV a utilizar.
-    id_length : int, optional
-        Número de Caracteres para utilizar en el Identificador de la serie de tiempo, por defecto 4.
-
-    Returns
-    -------
-    DataFrame
-        DataFrame con todos las series de tiempo.
-    """    
-    ts_id = file.split('_')[-1].split('.')[0][-id_length:]
-    output = pd.read_csv(file, index_col=0, parse_dates = ['date']).assign(ts_id = ts_id)
-    return output
+import numpy as np
+import pandas as pd
 
 
 class AnalyzeResample:
@@ -27,7 +11,6 @@ class AnalyzeResample:
     """    
     def __init__(self, df, rule):
         """
-
         Parameters
         ----------
         df : DataFrame
@@ -39,13 +22,15 @@ class AnalyzeResample:
         self.rule = rule
 
     def resample(self):
-        """Genera el Resample a probar.
-        """        
+        """Genera el Resample a probar generando las mismas variables del dataset Original.
+        """
+        variables = ['date', 'ndvi', 'ts_id', 'ndvi_null', 'id']
         self.df_resample = (self.df.groupby('ts_id')
             .resample(on = 'date', rule = self.rule).mean()
             .reset_index()
-            .assign(ndvi_null = lambda x: x.ndvi.isnull())
-        )
+            .assign(ndvi_null = lambda x: x.ndvi.isnull(),
+                    id = lambda x: x.ts_id + '-' + x.groupby('ts_id').date.rank().astype('int').astype(str)))[variables]
+        
 
     def plot_nulls(self):
         """Genera una comparación entre los Nulos resultantes entre el Resample y el Original.
@@ -75,7 +60,26 @@ class AnalyzeResample:
         self.plot_nulls()
         self.hist_nulls()
         return self.df_resample
-    
+
+
+def import_file(file, id_length = 4):
+    """Importa todos los archivos CSV de prueba convirtiendolo en un sólo DataFrame.
+
+    Parameters
+    ----------
+    file : str
+        Path que apunta a alguno de los archivos CSV a utilizar.
+    id_length : int, optional
+        Número de Caracteres para utilizar en el Identificador de la serie de tiempo, por defecto 4.
+
+    Returns
+    -------
+    DataFrame
+        DataFrame con todos las series de tiempo.
+    """    
+    ts_id = file.split('_')[-1].split('.')[0][-id_length:]
+    output = pd.read_csv(file, index_col=0, parse_dates = ['date']).assign(ts_id = ts_id)
+    return output
 
 
 def plot_examples(df, threshold):
@@ -92,6 +96,7 @@ def plot_examples(df, threshold):
     for index, value in zip(idxs.index, idxs):
         df.set_index('date').query('ts_id == @index').plot(figsize = (10,8), title = f'TS = [{index}] con {value} Nulos')
 
+
 def ts_stats(df):
     """Cálculo de Estadísticas de interés.
 
@@ -105,3 +110,87 @@ def ts_stats(df):
     print(f'Número de TSs: {df.ts_id.nunique()}')
     print(f'Largo Promedio por TS: {largo_prom}')
     print(f'Promedio de Nulos por TS: {nulos}')
+    
+    
+def show_original_vs_resamples(df_real, df_resamples, k, figsize = (30,40)):
+    """Función para comparar k series de tiempos provenientes de atasets, 
+    normalmente el original versus el Resampleado. 
+    
+    Parameters
+    ----------
+    df_real : DataFrame
+        DataFrame con la data sin resamplear.
+    df_resamples : DataFrame
+        DataFrame con la data resampleada.
+    k : int
+        Número de Series de Tiempo a mostrar.
+    figsize : tuple, optional
+        Tamaño del Gráfico a mostrar, by default (30,40)
+    """    
+    ids = df_resamples.ts_id.unique()
+    ids_selected = random.choices(ids, k = k)
+
+    fig, axes = plt.subplots(nrows=10, ncols=2, figsize=figsize)
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.1, hspace=1)
+
+    for n, id in enumerate(ids_selected):
+        df_real.set_index('date').query('ts_id == @id').plot(title = f'Ejemplos TS = {id} Original', ax = axes[n,0])
+        df_resamples.set_index('date').query('ts_id == @id').plot(title = f'Ejemplos TS = {id} con Resample',ax = axes[n,1])
+
+
+def show_k_sequences(sequences, dates, k = 10, figsize = (10,20)):
+    """Grafica K Secuencias aleatorias para ver su comportamiento.
+
+    Parameters
+    ----------
+    sequences : Numpy Array
+        Secuencias a graficar con Forma (N_seq x Seq_Len)
+    dates : Numpy Array
+        Fechas asociadas a las secuencias con Forma (N_seq x Seq_Len)
+    k : int, optional
+        Número de Secuencias a graficar, by default 10
+    figsize : tuple, optional
+        Tamaño del Gráfico, by default (10,20)
+    """    
+    VALUES = np.random.randint(0,len(sequences),k)
+
+    plt.figure(figsize = figsize)
+    for k, id in enumerate(VALUES, start = 1):
+        plt.subplot(10,1,k)
+        plt.plot(dates[id], sequences[id])
+
+def create_sequences(df, seq_len):
+    sequences = []
+    divisor = []
+    indices = []
+    dates = []
+    target = []
+    dates_target = []
+    ids = df.ts_id.unique()
+    for id in ids:
+        ts = df.query('ts_id == @id').to_numpy()
+
+        subsequences = np.zeros((len(ts)-seq_len+1, seq_len))
+        subindices = np.empty((len(ts)-seq_len+1, seq_len), dtype = 'object')
+        subdates = np.empty((len(ts)-seq_len+1, seq_len), dtype = 'datetime64[D]')
+        subdivisor = np.zeros(len(ts))
+        
+        for i in range(len(ts)-seq_len+1):
+            subsequences[i] = ts[i:i+seq_len,1]
+            subindices[i] = ts[i:i+seq_len,4]
+            subdates[i] = ts[i:i+seq_len,0]
+            subdivisor[i:i+seq_len] += np.ones(seq_len)
+            
+            
+            target.append(ts[i+seq_len,1] if i != len(ts)-seq_len else np.nan )
+            dates_target.append(ts[i+seq_len,0] if i != len(ts)-seq_len else np.nan )
+            
+        sequences.extend(subsequences)
+        divisor.append(subdivisor)
+        indices.extend(subindices)
+        dates.extend(subdates)
+        
+
+    print(f'Se crearon {len(sequences)} secuencias y {len(target)} targets.')
+        
+    return np.array(sequences), divisor, np.array(indices), np.array(dates), np.array(target), np.array(dates_target)
